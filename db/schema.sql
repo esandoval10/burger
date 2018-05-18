74a10ba618781cfe235cbd506887f7ffd008a685
Edgar Sandoval <ESE1307@Edgars-MacBook-Air.local>
2018-05-17 19:24:01 -0500

    First Attempt

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+drop database if exists burgers_db; 
+create database burgers_db;
+use burgers_db;
+
+create table burgers(
+    id int auto_increment not null,
+    burger_name varchar(255) not null, 
+    devoured boolean default false,
+    primary key (id) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) values ("Hawaiian Burger");
+insert into burgers (burger_name) values ("Cheese Burger");
+insert into burgers (burger_name) values ("Bacon Lovers");
